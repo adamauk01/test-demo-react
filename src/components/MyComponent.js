f9ea5cecf71aab7870a3c528db33c77d44d10760
vanh601 <vanh18601k@gmail.com>
2024-03-29 18:03:18 +0700

    bai-2

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,10 +2,15 @@
 // function component
 import React from "react";
 class MyComponent extends React.Component{
+  state = {
+    name: 'eric',
+    address: 'hoi dan it',
+    age: 26
+  }
   //JSX
   render() {
     return (
-      <div>my first component {Math.random()}</div>
+      <div>My name is {this.state.name} and i'm from {this.state.address}</div>
     );
   }
 }
